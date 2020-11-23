20f88f6e55c560258ef284016db6a8dd0edde28c
malikdawar <malikdawar@hotmail.com>
2020-11-23 22:56:05 +0300

    # Developed the required view extensions # Add the string in string.xml

---
+++ app/src/main/java/com/example/noteapp/extensions/ViewExtension.kt
@@ -0,0 +1,45 @@
+package com.example.noteapp.extensions
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.example.noteapp.App
+
+/**
+ * Extension function to show toast message
+ * @return void
+ * @author Dawar Malik.
+ */
+fun Any.showToastMsg(message: String) {
+    Toast.makeText(App.getAppContext(), message, Toast.LENGTH_SHORT).show()
+}
+
+/**
+ * An Extension to make view Visible
+ * @return void
+ * @author Dawar Malik.
+ */
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+/**
+ * An Extension to make view Gone
+ * @return void
+ * @author Dawar Malik.
+ */
+fun View.gone() {
+    visibility = View.GONE
+}
+
+/**
+ * An Extension to close keyboard.
+ * @return void
+ * @author Dawar Malik.
+ */
+fun View.closeKeyboard() {
+    val inputMethodManager =
+        this.context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
+}
